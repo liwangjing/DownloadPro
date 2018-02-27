64c9d1707545c3396f48b581e3bc36bddbd77e67
jingli <jli229@uic.edu>
2018-02-27 10:01:50 -0600

    add activity and service

---
+++ app/src/main/java/com/example/self/practicetest/DownloadService.java
@@ -0,0 +1,140 @@
+package com.example.self.practicetest;
+
+
+import android.app.AliasActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class DownloadService extends Service{
+    private static final String APPTAG = "Download Service";
+
+    private DownloadTask mDownloadTask;
+
+    private String mDownloadUrl;
+
+    private DownloadBinder mBinder;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private DownloadListener mListener = new DownloadListener() {
+        @Override
+        public void onProgress(int progress) {
+            Log.e(APPTAG, "on progress");
+            getNotificationManager().notify(1, getNotification("Downloading...", progress));
+        }
+
+        @Override
+        public void onSuccess() {
+            Log.e(APPTAG, "onSuccess");
+            mDownloadTask = null;
+            stopForeground(true);  // 下载成功时，将前台服务通知关闭，并创建一个下载成功通知
+            getNotificationManager().notify(1, getNotification("Download Success", -1));
+            makeToast("Download Success");
+        }
+
+        @Override
+        public void onFailed() {
+            // 下载失败了
+            Log.e(APPTAG, "onFailed");
+            mDownloadTask = null;
+
+            stopForeground(true);  // 下载成功时，将前台服务通知关闭，并创建一个下载成功通知
+            getNotificationManager().notify(1, getNotification("Download Failed", -1));
+            makeToast("Download Failed");
+        }
+
+        @Override
+        public void onPaused() {
+            Log.e(APPTAG, "onPaused");
+            mDownloadTask = null;
+            makeToast("Download Paused");
+        }
+
+        @Override
+        public void onCanceled() {
+            Log.e(APPTAG, "onFailed");
+            mDownloadTask = null;
+            stopForeground(true);  // 下载成功时，将前台服务通知关闭，并创建一个下载成功通知
+            makeToast("Download Failed");
+        }
+    };
+
+    class DownloadBinder extends Binder{
+
+        public void startDownload(String url) {
+            if (mDownloadTask == null) {
+                Log.e(APPTAG, "startDownload");
+                mDownloadUrl = url;
+                mDownloadTask = new DownloadTask(mListener);
+                mDownloadTask.execute();
+                startForeground(1, getNotification("Downloading...", 0));
+                makeToast("start download...");
+            }
+        }
+
+        public void pauseDownload() {
+            if (mDownloadTask != null) {
+                Log.e(APPTAG, "pauseDownload");
+                mDownloadTask.pauseDownload();
+            }
+        }
+
+        public void cancelDownload() {
+            Log.e(APPTAG, "cancelDownload");
+            if (mDownloadTask != null) {
+                mDownloadTask.cancelDownload();
+            }
+            if (mDownloadUrl != null) {
+                String fileName = mDownloadUrl.substring(mDownloadUrl.lastIndexOf("/"));
+                String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                File file = new File(directory + fileName);
+                if (file.exists()) {
+                    file.delete();
+                }
+                getNotificationManager().cancel(1);
+                stopForeground(true);
+                makeToast("Cancel Download");
+            }
+        }
+    }
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pendingIntent);
+        builder.setContentTitle(title);
+        if (progress > 0) {
+            builder.setContentText(progress + "%");
+            builder.setProgress(100, progress, false);
+        }
+        return builder.build();
+    }
+
+    private void makeToast(String string) {
+        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/self/practicetest/MainActivity.java
+++ app/src/main/java/com/example/self/practicetest/MainActivity.java
@@ -1,19 +1,51 @@
 package com.example.self.practicetest;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     AppCompatButton mStart, mPause, mCancel;
 
+    private DownloadService.DownloadBinder mBinder;
+    private Intent intent;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mBinder = (DownloadService.DownloadBinder) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+        intent = new Intent(this, DownloadService.class);
+        startService(intent);
+
+        bindService(intent, mConnection, BIND_AUTO_CREATE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
     }
 
     private void initView() {
@@ -24,23 +56,55 @@ public class MainActivity extends AppCompatActivity {
         mStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (mBinder != null) {
+                    String url = "";
+                    mBinder.startDownload(url);
+                }
             }
         });
 
         mPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (mBinder != null) {
+                    mBinder.pauseDownload();
+                }
             }
         });
 
         mCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (mBinder != null) {
+                    mBinder.cancelDownload();
+                }
             }
         });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    makeToast("permission denied..");
+                }
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mConnection);
+        stopService(intent);
+    }
+
+    private void makeToast(String string) {
+        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
     }
 }
